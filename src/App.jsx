65203cb6f3797f8db4a326ac5a5aa355dcc77af3
Maksym Prokopovych <prokopovych2006@gmail.com>
2025-01-06 15:26:39 +0200

    fix-bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.scss';
 import { Footer } from './components/Footer/Footer';
-// import { Header } from './components/Header/Header';
+import { Header } from './components/Header/Header';
 import { MainPage } from './components/MainPage/MainPage';
 
 export const App = () => {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <MainPage />
       <Footer />
     </div>
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,23 +1,78 @@
+import { useEffect, useState } from 'react';
 import './MainPage.scss';
 
 export const MainPage = () => {
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  function calculateTimeLeft() {
+    const targetDate = new Date('2025-01-08T00:00:00'); // 8 січня 2025
+    const now = new Date();
+    const difference = targetDate - now;
+
+    if (difference <= 0) {
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
+    }
+
+    return {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
   return (
     <main className="main">
       <div className="main__paddings container">
-          <div className="main__inner">
-            <div className="main__inner_info">
-              <h2 className="main__inner_info-title">Just sign up to win a smart watch.</h2>
-              <p className="main__inner_info-text">Enter our monthly drawing 
-                for a chance to win one of five smart watches. 
-                Contest winners will be announced on the first day 
-                of every month.
-              </p>
-              <button className="main__inner_info-button">I want to win!</button>
+        <div className="main__inner">
+          <div className="main__inner_info">
+            <h2 className="main__inner_info-title">Just sign up to win a smart watch.</h2>
+            <p className="main__inner_info-text">Enter our monthly drawing
+              for a chance to win one of five smart watches.
+              Contest winners will be announced on the first day
+              of every month.
+            </p>
+            <button className="main__inner_info-button">I want to win!</button>
+          </div>
+          <div className="main__inner_image">
+            <img src='/img/watch.webp' alt="images" />
+          </div>
+        </div>
+
+        <div className="main__timer">
+          <div className="main__timer-countdown">
+            <div className="main__timer-item">
+              <span className="main__timer-item-time">{timeLeft.days}</span>
+              <span className="main__timer-item-text">Days</span>
             </div>
-            <div className="main__inner_image">
-              <img src="img/watch.webp" alt="images" />
+            <div className="main__timer-item">
+              <span>{timeLeft.hours}</span>
+              <span>Hours</span>
+            </div>
+            <div className="main__timer-item">
+              <span>{timeLeft.minutes}</span>
+              <span>Minutes</span>
+            </div>
+            <div className="main__timer-item">
+              <span>{timeLeft.seconds}</span>
+              <span>Seconds</span>
             </div>
           </div>
+          <p className="main__timer_text">until the contest ends!</p>
+        </div>
       </div>
     </main>
   );
